f277d6847b8127d72e55e5ebb217b1430e406edc
Paolo Manca <paolo.manca@pagopa.it>
2024-05-06 08:59:23 +0200

    push signal - wip

--- features/push-signals/push-signals.feature
+++ features/push-signals/push-signals.feature
@@ -2,6 +2,10 @@
 Feature: Deposito segnali
   Tutti gli utenti autorizzati all'e-service deposito segnali possono depositare segnali.
 
+  Un utente, che ha un ruolo <ruolo>, di un ente aderente <ente>, come erogatore, pubblica un un e-service <e-service>.  L'utente, il cui ente aderente rispetta i requisiti, ha una richiesta di fruizione attiva per l'e-service deposito segnali. L'utente ha una finalità attiva per l'e-service deposito segnali.
+  L'utente ha un client associato alla finalità. L’utente dell’ente aderente diventa produttore di segnali.
+
+
   @push_signals1
   Scenario Outline: Un utente abilitato all'e-service deposito segnali deposita un segnale. La richiesta va a buon fine.
     Given Un utente, come produttore di segnali, ottiene un voucher valido per l’accesso all'e-service deposito segnali
---
+++ utils/common.ts
@@ -0,0 +1,123 @@
+import { v4 as uuidv4 } from "uuid";
+import { importPKCS8, SignJWT } from "jose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export type VoucherPayload = {
+  client_id: string;
+  grant_type: string;
+  client_assertion: string;
+  client_assertion_type: string;
+};
+
+export async function obtainVoucher(
+  voucherPayloadOptions: VoucherPayload,
+  urlAuthentication: string
+): Promise<string> {
+  const formData = new URLSearchParams();
+  for (const [key, value] of Object.entries(voucherPayloadOptions)) {
+    // console.log(`Key: ${key}, Value: ${value}`);
+    formData.append(`${key}`, value);
+  }
+  const response = await fetch(urlAuthentication, {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      accept: "application/json",
+    },
+    body: formData,
+  });
+  if (response.status === 200) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const data: any = await response.json();
+    // console.log('dump voucher', data);
+    return data.access_token;
+  }
+  return "";
+}
+
+export type JWTHeader = {
+  alg: string;
+  typ: string;
+  kid: string;
+};
+
+export type JWTPayload = {
+  sub: string;
+  iss: string;
+  aud: string;
+  jti: string;
+  purposeId: string;
+  iat: number;
+  exp: number;
+};
+
+export async function generateClientAssertion(
+  jwtHeaderOptions: JWTHeader,
+  jwtPayloadOptions: JWTPayload,
+  privateKeyPemEncoded: string
+) {
+  // Load the private key from the protected keystore
+  const { alg } = jwtHeaderOptions;
+  const privateKey = await importPKCS8(privateKeyPemEncoded, alg);
+
+  // Create a JWT client assertion signed with the private key
+  // console.log('client assertion', assertion);
+  return await new SignJWT(jwtPayloadOptions)
+    .setProtectedHeader(jwtHeaderOptions)
+    .sign(privateKey);
+}
+
+export const getVoucher = async (): Promise<string> => {
+  try {
+    const jwtHeader: JWTHeader = buildJWTHeader();
+    const jwtPayload: JWTPayload = buildJWTPayload();
+    const privateKeyPem = process.env.SH_PUSH_PRIVATE_KEY ?? "";
+    const clientAssertion = await generateClientAssertion(
+      jwtHeader,
+      jwtPayload,
+      privateKeyPem
+    );
+    const voucherPayload: VoucherPayload = buildVoucherPayload(clientAssertion);
+    // console.log("voucherPayload", voucherPayload, process.env.URL_AUTH_TOKEN);
+    return await obtainVoucher(
+      voucherPayload,
+      process.env.URL_AUTH_TOKEN ?? ""
+    );
+  } catch (err) {
+    console.log(err);
+    throw new Error("no voucher");
+  }
+
+  function buildVoucherPayload(clientAssertion: string): VoucherPayload {
+    return {
+      client_id: process.env.CLIENT_ID ?? "",
+      grant_type: process.env.GRANT_TYPE ?? "",
+      client_assertion: clientAssertion,
+      client_assertion_type: process.env.ASSERTION_TYPE ?? "",
+    };
+  }
+};
+
+function getIssuedAtTime(): number {
+  return Math.round(new Date().getTime() / 1000);
+}
+function buildJWTPayload(): JWTPayload {
+  return {
+    iss: process.env.ISSUER ?? "",
+    sub: process.env.SUBJECT ?? "",
+    aud: process.env.AUDIENCE ?? "",
+    jti: uuidv4(),
+    purposeId: process.env.PURPOSE_ID ?? "",
+    iat: getIssuedAtTime(),
+    exp: getIssuedAtTime() + Number(process.env.SESSION_DURATION_IN_SECONDS),
+  };
+}
+
+function buildJWTHeader(): JWTHeader {
+  return {
+    alg: "RS256",
+    typ: "JWT",
+    kid: process.env.KEY_ID ?? "",
+  };
+}
