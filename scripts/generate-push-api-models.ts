eb2c0c046f273bd1837788228b215368245afba8
borgesis95 <salvatore.borgesi@pagopa.it>
2024-09-26 16:03:50 +0200

    refactor: updated openAPI url for push,pull (#39)

--- scripts/generate-push-api-models.ts
+++ scripts/generate-push-api-models.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { generateApi } from "swagger-typescript-api";
 
 const openApiSpecificationFileUrl =
-  "https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/NTRP-313_openapi_info/packages/push-signal/src/api/push-signals_1.2.0_.yaml";
+  "https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/packages/push-signal/src/api/push-signals_1.1.0.yaml";
 
 const apiFolderPath = path.resolve("./api/");
 
