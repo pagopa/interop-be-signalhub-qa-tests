aa4ffd4e391811b14f1af98d98580db260d657b7
Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>
2024-10-11 16:38:03 +0200

    NTRP-353 implementation step file pull signals (#43)
    * implementation step file
    
    * define steps
    
    * implementing steps and adding function to common lib
    
    * restructure agreement and purpose creation

--- features/pull-signals/step_definitions/pull-signals.ts
+++ features/pull-signals/step_definitions/pull-signals.ts
@@ -2,71 +2,28 @@ import assert from "assert";
 import { Given, Then, When } from "@cucumber/cucumber";
 import {
   assertValidResponse,
+  createAgreement,
   createPullSignalRequest,
+  createPurpose,
   createSignal,
-  eserviceIdAgreementSuspendedWithConsumer,
-  eserviceIdNotAgreementWithConsumer,
+  getAgreement,
   getAuthorizationHeader,
+  getConsumerOrganization,
+  getPurpose,
   sleep,
 } from "../../../lib/common";
 import { pullSignalApiClient } from "../../../api/pull-signal.client";
 import { pushSignalApiClient } from "../../../api/push-signals.client";
-import { getExpiredVoucher, getVoucherBy } from "../../../lib/voucher";
+import { getExpiredVoucher } from "../../../lib/voucher";
 import { VoucherTypologies } from "../../../lib/voucher.env";
 
-Given("il sistema deposita (il)(i) segnal(e)(i)", async function () {
-  // This sleep functioxn simulate the time SQS will take to process the signal and put on DB
+Given("il sistema ha depositato (il)(i) segnal(e)(i)", async function () {
+  // This sleep function simulate the time SQS will take to process the signal and put on DB
   await sleep(10000);
 });
 
 Given(
-  "un utente, come produttore di segnali, ottiene un voucher valido per l'accesso all'e-service deposito segnali",
-  async function () {
-    const voucher = await getVoucherBy(VoucherTypologies.Enum.PRODUCER);
-    this.producerVoucher = voucher;
-  }
-);
-
-Given(
-  "un utente, come consumatore di segnali, ottiene un voucher valido per l'accesso all'e-service lettura segnali",
-  async function () {
-    const voucher = await getVoucherBy(VoucherTypologies.Enum.CONSUMER);
-    this.consumerVoucher = voucher;
-  }
-);
-
-Given(
-  "un utente, come consumatore di segnali, ottiene un voucher valido per un e-service diverso dall'e-service di lettura segnali",
-  async function () {
-    const voucher = await getVoucherBy(VoucherTypologies.Enum.PRODUCER);
-    this.consumerVoucher = voucher;
-  }
-);
-
-Given(
-  "un utente, come consumatore di segnali, ottiene un voucher scaduto per l'accesso all'e-service lettura segnali",
-  async function () {
-    const expired = await getExpiredVoucher(VoucherTypologies.Enum.CONSUMER);
-    this.consumerVoucher = expired;
-  }
-);
-
-When("l'utente consumatore recupera (un)(i) segnal(e)(i)", async function () {
-  // If SignalId is not present in previous given start by signalId = 1
-  const signalId = (this.startSignalId || 1) - 1;
-  const pullSignalRequest = createPullSignalRequest({
-    signalId,
-    size: 100,
-  });
-
-  this.response = await pullSignalApiClient.v1.pullSignal(
-    pullSignalRequest,
-    getAuthorizationHeader(this.consumerVoucher)
-  );
-});
-
-Given(
-  "l'utente produttore di segnali deposita {int} segnal(e)(i)",
+  "un utente produttore di segnali ha depositato {int} segnal(e)(i)",
   async function (signalLength: number) {
     const startSignalId = 1;
     const signalRequest = createSignal({
@@ -92,16 +49,73 @@ Given(
   }
 );
 
-When(
-  "l'utente consumatore recupera un segnale per un e-service con cui non ha una richiesta di fruizione",
+Given(
+  "l'utente consumatore di segnali ha ottenuto un voucher api scaduto",
   async function () {
+    const voucherExpired = await getExpiredVoucher(
+      VoucherTypologies.Enum.CONSUMER
+    );
+    this.voucher = voucherExpired;
+  }
+);
+
+Given(
+  "l'utente ha già una richiesta di fruizione in stato {string} per l'e-service {string}",
+  async function (agreementStatus: string, eserviceName: string) {
+    const organization = getConsumerOrganization();
+    const agreement = getAgreement(eserviceName);
+    return await createAgreement(
+      {
+        ...agreement,
+        ...{ state: agreementStatus },
+      },
+      organization
+    );
+  }
+);
+
+Given(
+  "l'utente ha già una finalità in stato {string} per l'e-service {string}",
+  async function (purposeStatus: string, eserviceName: string) {
+    const organization = getConsumerOrganization();
+    const purpose = getPurpose(eserviceName);
+    return await createPurpose(
+      {
+        ...purpose,
+        ...{ state: purposeStatus },
+      },
+      organization
+    );
+  }
+);
+
+When(
+  "l'utente recupera (un)(i) segnal(e)(i) dell'e-service {string}",
+  async function () {
+    // If SignalId is not present in previous given start by signalId = 1
+    const signalId = (this.startSignalId || 1) - 1;
     const pullSignalRequest = createPullSignalRequest({
-      eserviceId: eserviceIdNotAgreementWithConsumer,
+      signalId,
+      size: 100,
     });
 
     this.response = await pullSignalApiClient.v1.pullSignal(
       pullSignalRequest,
-      getAuthorizationHeader(this.consumerVoucher)
+      getAuthorizationHeader(this.voucher)
+    );
+  }
+);
+
+When(
+  "l'utente recupera un segnale dell'e-service {string} con un signalId uguale a {int}",
+  async function (startSignalId: number) {
+    const pullSignalRequest = createPullSignalRequest({
+      signalId: startSignalId,
+    });
+
+    this.response = await pullSignalApiClient.v1.pullSignal(
+      pullSignalRequest,
+      getAuthorizationHeader(this.voucher)
     );
   }
 );
@@ -110,35 +124,7 @@ When(
   "l'utente verifica lo stato del servizio di recupero segnali",
   async function () {
     this.response = await pullSignalApiClient.v1.getStatus(
-      getAuthorizationHeader(this.consumerVoucher)
-    );
-  }
-);
-
-When(
-  "l'utente consumatore recupera un segnale per un e-service con cui ha una richiesta di fruizone in stato diverso da ACTIVE",
-  async function () {
-    const pullSignalRequest = createPullSignalRequest({
-      eserviceId: eserviceIdAgreementSuspendedWithConsumer,
-    });
-
-    this.response = await pullSignalApiClient.v1.pullSignal(
-      pullSignalRequest,
-      getAuthorizationHeader(this.consumerVoucher)
-    );
-  }
-);
-
-When(
-  "l'utente consumatore recupera un segnale inserendo un signalId uguale a {int}",
-  async function (startSignalId: number) {
-    const pullSignalRequest = createPullSignalRequest({
-      signalId: startSignalId,
-    });
-
-    this.response = await pullSignalApiClient.v1.pullSignal(
-      pullSignalRequest,
-      getAuthorizationHeader(this.consumerVoucher)
+      getAuthorizationHeader(this.voucher)
     );
   }
 );
@@ -160,17 +146,6 @@ Then(
 
     assert.strictEqual(data.signals?.length, signalsLength);
     assert.strictEqual(data.lastSignalId, lastSignalId);
-    assert.strictEqual(this.response.status, statusCode);
-  }
-);
-
-Then(
-  "la richiesta va a buon fine con status code {int} e restituisce una lista di {int} segnal(e)(i) e nessun lastSignalId",
-  function (statusCode: number, numberOfSignalList: number) {
-    const data = this.response.data;
-
-    assert.strictEqual(data.signals?.length, numberOfSignalList);
-    assert.strictEqual(data.lastSignalId, null);
     assert.strictEqual(this.response.status, statusCode);
   }
 );
--- lib/common.ts
+++ lib/common.ts
@@ -3,8 +3,29 @@ import fs from "fs";
 import { AxiosResponse } from "axios";
 import { SignalPayload, SignalType } from "../api/push-signals.models";
 import { PullSignalParams } from "../api/pull-signals.models";
+import {
+  clientSchemaInteropAgreement,
+  clientSchemaInteropPurpose,
+} from "../data/db.data.preparation";
 
 const SIGNAL_TYPE_DEFAULT: SignalType = "CREATE";
+
+type Agreement = {
+  id: string;
+  state: string;
+  eservice: string;
+  name: string;
+  descriptor: string;
+  purpose: string;
+};
+
+type Purpose = {
+  id: string;
+  version: string;
+  state: string;
+  eservice: string;
+  name: string;
+};
 
 function getActors() {
   const catalogInteropData = JSON.parse(
@@ -13,43 +34,37 @@ function getActors() {
     ).toString()
   );
   const signalProducer = catalogInteropData.PRODUCERS[0].organization;
-  const eserviceProducer = catalogInteropData.PRODUCERS[1].organization;
+  const anotherSignalProducer = catalogInteropData.PRODUCERS[1].organization;
   const signalConsumer = catalogInteropData.CONSUMERS[0].organization;
+
   const eserviceIdPushSignals = signalProducer.eservices[0].id;
   const eserviceIdSecondPushSignals = signalProducer.eservices[1].id;
-  const eserviceIdNotAgreementWithConsumer = signalProducer.eservices[1].id;
   const eserviceIdAgreementSuspendedWithConsumer =
     signalProducer.eservices[2].id;
-  const eserviceIdNotPublished = signalProducer.eservices[3].id;
+  const eserviceIdNotPublished = signalProducer.eservices[2].id;
   const eserviceIdPublishedByAnotherOrganization =
-    eserviceProducer.eservices[0].id;
-  const purposeIdDifferentFromEservicePushSignals =
-    signalProducer.agreements[1].purpose;
+    anotherSignalProducer.eservices[0].id;
+
   return {
     signalProducer,
     signalConsumer,
-    eserviceProducer,
+    anotherSignalProducer,
     eserviceIdPushSignals,
     eserviceIdSecondPushSignals,
-    eserviceIdNotAgreementWithConsumer,
     eserviceIdAgreementSuspendedWithConsumer,
     eserviceIdNotPublished,
     eserviceIdPublishedByAnotherOrganization,
-    purposeIdDifferentFromEservicePushSignals,
   };
 }
 
 export const {
   signalProducer,
   signalConsumer,
-  eserviceProducer,
   eserviceIdPushSignals,
-  eserviceIdSecondPushSignals,
-  eserviceIdNotAgreementWithConsumer,
-  eserviceIdAgreementSuspendedWithConsumer,
+  anotherSignalProducer,
   eserviceIdNotPublished,
   eserviceIdPublishedByAnotherOrganization,
-  purposeIdDifferentFromEservicePushSignals,
+  eserviceIdSecondPushSignals,
 } = getActors();
 
 export function getAuthorizationHeader(token: string) {
@@ -64,6 +79,49 @@ export function assertValidResponse<T>(response: AxiosResponse<T>) {
       )}`
     );
   }
+}
+
+export function getConsumerOrganization(): string {
+  const { id } = signalConsumer;
+  return id;
+}
+
+export function getAgreement(eservice: string): Agreement {
+  const { agreements } = signalConsumer;
+  return agreements
+    .filter((agreement: Agreement) => agreement.name === eservice)
+    .shift();
+}
+
+export async function createAgreement(
+  agreement: Agreement,
+  organizationId: string
+): Promise<void> {
+  const { id, eservice, descriptor, state } = agreement;
+  const query = {
+    text: "INSERT INTO dev_interop.agreement (agreement_id, eservice_id, consumer_id, descriptor_id, state) values ($1, $2, $3, $4, $5) ON CONFLICT(agreement_id) DO NOTHING",
+    values: [id, eservice, organizationId, descriptor, state],
+  };
+  await clientSchemaInteropAgreement.query(query);
+}
+
+export function getPurpose(eservice: string): Purpose {
+  const { purposes } = signalConsumer;
+  return purposes
+    .filter((purpose: Purpose) => purpose.name === eservice)
+    .shift();
+}
+
+export async function createPurpose(
+  purpose: Purpose,
+  organizationId: string
+): Promise<void> {
+  const { state, version, eservice, id } = purpose;
+  const query = {
+    text: "INSERT INTO DEV_INTEROP.purpose(purpose_id, purpose_version_id, purpose_state, eservice_id, consumer_id) values ($1, $2, $3, $4, $5) ON CONFLICT(purpose_id) DO NOTHING",
+    values: [id, version, state, eservice, organizationId],
+  };
+  await clientSchemaInteropPurpose.query(query);
 }
 
 export function createSignal(
@@ -88,21 +146,6 @@ export function createPullSignalRequest(
     ...partialPullSignalRequest,
   };
 }
-
-// export function createSignalConsumers(): SignalConsumerRequest {
-//   return {
-//     signalByConsumers: [
-//       {
-//         consumerId: signalConsumer.id,
-//         objectId: crypto.randomUUID(),
-//       },
-//     ],
-//     signalType: SIGNAL_TYPE_DEFAULT,
-//     eserviceId: eserviceIdPushSignals,
-//     objectType: crypto.randomUUID(),
-//     signalId: getRandomSignalId(),
-//   };
-// }
 
 export function getRandomSignalId() {
   return parseInt(
