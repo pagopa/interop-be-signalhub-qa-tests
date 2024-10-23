dcba1f531a6c66af78e3994b51da775a113eff6c
Paolo Manca <paolo.manca@pagopa.it>
2024-10-23 17:33:39 +0200

    before/after delete

--- features/before-and-after.ts
+++ features/before-and-after.ts
@@ -1,4 +1,5 @@
 import {
+  After,
   AfterAll,
   Before,
   BeforeAll,
@@ -8,7 +9,7 @@ import { cleanupQAData } from "../data/db";
 import { nodeEnv } from "../configs/env";
 import { getRandomInt } from "../lib/common";
 
-const PATTERN_FOR_DELETE_QA_DATA = `QA-%|%`;
+const PATTERN_FOR_DELETE_ALL_QA_DATA = `QA-%|%`;
 
 // Increase duration of every step with the following timeout (Default is 5000 milliseconds)
 setDefaultTimeout(process.env.CUCUMBER_SET_DEFAULT_TIMEOUT_MS);
@@ -17,15 +18,17 @@ BeforeAll(async function () {
   console.info(`\n*** BEGIN SIGNALHUB QA TEST SUITE IN ENV [${nodeEnv}] ***`);
 });
 
-// see https://cucumber.io/docs/cucumber/state/?lang=javascript
 Before(async function () {
-  await cleanupQAData(PATTERN_FOR_DELETE_QA_DATA);
   const seed = `QA-${getRandomInt()}|`;
   this.TEST_SEED = seed;
 });
 
+After(async function () {
+  await cleanupQAData(this.TEST_SEED);
+});
+
 AfterAll(async function () {
-  await cleanupQAData(PATTERN_FOR_DELETE_QA_DATA);
+  await cleanupQAData(PATTERN_FOR_DELETE_ALL_QA_DATA);
   console.info(`CLEANING UP ALL QA DATA`);
   console.info(`*** END SIGNALHUB QA TEST SUITE IN ENV [${nodeEnv}] ***\n`);
 });
